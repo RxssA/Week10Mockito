ba90a67a22307fec361c16fd8f1b5c54a7d09326
rossa <g00401655@atu.ie>
2024-12-26 15:49:31 +0000

    removed unused imports, checked everything is finished

--- src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
@@ -1,14 +1,13 @@
 package ie.atu.week8.projectexercise;
 
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
